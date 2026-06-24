569c2205cb0603c7ea829ad6df442f2195272ac3
lu1234566 <barcelar34@gmail.com>
2026-06-24 01:30:59 -0300

    Add native preferences types

---
+++ types/preferences.ts
@@ -0,0 +1,8 @@
+export interface ReaderPreferences {
+  readerName: string;
+  yearlyGoal: number;
+  dailyPageGoal: number;
+  favoriteFormat: string;
+  reminderText: string;
+  syncUserId: string;
+}
