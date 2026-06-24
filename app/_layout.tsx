74c3825c2f808a334816880429bc2c0fdde8fd4f
lu1234566 <barcelar34@gmail.com>
2026-06-23 23:26:35 -0300

    Add Expo Router root layout

---
+++ app/_layout.tsx
@@ -0,0 +1,23 @@
+import { Stack } from 'expo-router';
+import { BookProvider } from '@/contexts/BookContext';
+import { appColors } from '@/theme/tokens';
+
+export default function RootLayout() {
+  return (
+    <BookProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: appColors.background },
+          headerTintColor: appColors.gold,
+          headerTitleStyle: { fontWeight: '800' },
+          contentStyle: { backgroundColor: appColors.background }
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: 'Readora' }} />
+        <Stack.Screen name="library" options={{ title: 'Biblioteca' }} />
+        <Stack.Screen name="add" options={{ title: 'Adicionar livro' }} />
+        <Stack.Screen name="book/[id]" options={{ title: 'Detalhes' }} />
+      </Stack>
+    </BookProvider>
+  );
+}
